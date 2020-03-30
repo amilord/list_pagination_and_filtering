79f5eaa2dfac66db40f0edfec6f7fbd61cd5a379
amilord <60766420+amilord@users.noreply.github.com>
2020-03-30 00:27:32 -0400

    changes made to project to  meet expectations

--- js/script.js
+++ js/script.js
@@ -1,123 +1,94 @@
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-// I created two global variables which store the list items and number of students that will be displayed 
-const studentlistitems = document.getElementsByClassName ('student-item')
+// I created two global variables which store the list items and number of students that will be displayed
+const studentlistitems = document.getElementsByClassName("student-item");
 const itemsperpage = 10;
 
-
- 
-// this `showPage` function hides all of the items in each page list except for the ten that need to be shown.
-const showPage = (list, page) => { 
-   let start = (page * itemsperpage) - itemsperpage;
-   let end = (page * itemsperpage)-1;
+// this showPage function hides all of the items in each page list except for the ten that need to be shown.
+const showPage = (list, page) => {
+let start = page * itemsperpage - itemsperpage;
+let end = page * itemsperpage - 1;
 /// created a for loop for the paginagtion which will show only the items i wish to display///
-/// changed the error  in my for loop which stated <= list.length; instead of < list.length;
-   for(i=0; i < list.lenght; i++)
-   {
-      //Inside the loop, display any list item with if the
-      //index is greater than or equal to the start index variable and less than the end index variable.
-      if ( i>=start && i <= end){
-
-      list[i].style.display= 'block'
-
-} else{
-        list[i] .style.display= 'none'
+/// changed the error in my for loop which stated <= list.length; instead of < list.length;
+for (i = 0; i < list.length; i++) {
+//Inside the loop, display any list item with if the
+//index is greater than or equal to the start index variable and less than the end index variable.
+if (i >= start && i <= end) {
+list[i].style.display = "block";
+} else {
+list[i].style.display = "none";
 }
-   }
 }
+};
 
-const appendPageLinks = (list) => {
+const appendPageLinks = list => {
+// Determine how many pages are needed for the list by dividing the total number of items by the max number of items per page
+// used the math.ceil function to round the number passed as parameter to its nearest interger in upward direction towards the greater value
+const neededPages = Math.ceil(list.length / itemsperpage);
 
-   // Determine how many pages are needed for the list by  dividing the total number of items by the max number of items per page
-   // used the math.ceil function to round the number passed as parameter to its nearest interger in upward direction towards the greater value
-   const neededPages = Math.ceil(list.length / itemsperpage);
+const div = document.createElement("div");
 
+// In your appendPageLinks function you are trying to get the div
+// but it doesn't exist yet. I will create this.
 
-   const div = document.getElementsByClassName("div");
+// Give the div element a className of pagination in order for i
+div.className = "pagination";
 
-   
+// You are trying to select the ul but like the div you need
+// to create it then you can append it to the div you created.
 
-   // In your appendPageLinks function you are trying to get the div 
-   // but it doesn't exist yet. You should be creating it.
+// Add a ul to the “pagination” div to store the pagination links
+const ul = document.createElement("ul");
 
+// Append the newly created ul to the div with class of pagination
+div.appendChild(ul);
 
-   //  Give the div element a className of pagination in order for i
-   div.className = 'pagination';
+// Get hold of the parent div that the newly created div will be appended to
+// but I am using document.querySelector since I am just bringing back one element.
 
-   // You are trying to select the ul but like the div you need 
-  //  to create it then you can append it to the div you created.
+let parentDiv = document.querySelector(".page");
+parentDiv.appendChild(div);
 
-   //  Add a ul to the “pagination” div to store the pagination links
-   const ul = document.getElementsByClassName("ul");
+// Declare variable for <a></a> tags
+let links;
+// Loop over pagination links to remove active class from all links
 
+for (i = 1; i <= neededPages; i++) {
+// Create <a></a> elements
+links = document.createElement("a");
 
-   // Append the newly created ul to the div with class of pagination
-   div.appendChild(ul);
+// Create li element
+let li = document.createElement("li");
 
+// Give <a></a> elements href value of '#'
+links.href = "#";
 
-   // Get hold of the parent div that the newly created div will be appended to
-   // but I am using document.querySelector since I am just bringing back one element.
-   
-   let parentDiv = document.querySelector(".page");
-   parentDiv.appendChild(div);
+links.textContent = i;
 
+// Append the <a></a> elements variable (links) to the li
+li.appendChild(links);
 
-   // Declare variable for <a></a> tags
-   let links;
-   //  Loop over pagination links to remove active class from all links
+// li appeneds to the ul tht was created outside of the current for-loop
+ul.appendChild(li);
 
-         for(i = 1; i <= neededPages; i++){
-            
-   // Create <a></a> elements
-   links = document.createElement("a");
+// call the showPage function to display the appropriate page
+links.addEventListener("click", e => {
+const pageLinks = document.querySelectorAll(".pagination a");
 
-   // Create li element
-   let li = document.createElement('li');
-
-   // Give <a></a> elements href value of '#'
-   links.href = '#';
-
-    links.textContent = i;
-
-   // Append the <a></a> elements variable (links) to the li
-   li.appendChild(links)
-
-   // The li will then be appened to the ul tht was created outside of the current for-loop
-   ul.appendChild(li)
-                  
-
-   // Add event listener to <a></a> tags Add an event listener to each a tag. When they are clicked
-  //  call the showPage function to display the appropriate page
-   links.addEventListener('click', (e)=>{
-
-   const pageLinks = document.querySelectorAll('.pagination a')
-   
-   for(let i =0; i < pageLinks.length; i++){
-   pageLinks[i].className = ''
-   }
-   let activeClass = event.target;     
-   activeClass.className = 'active';  
-   showPage(list,event.target.textContent)
-
-   // Add the active class to the link that was just clicked. You can identify that
-   // clicked link using event.target
-                                 
-})
-
+for (let i = 0; i < pageLinks.length; i++) {
+pageLinks[i].className = "";
 }
+let activeClass = event.target;
+activeClass.className = "active";
+showPage(list, event.target.textContent);
 
+// Add the active class to the link that was just clicked. You can identify that
+// clicked link using event.target
+});
 }
-//  calling my functions
-showPage (studentlistitems,1)
-appendPageLinks(studentlistitems)
+};
+// calling my functions
+showPage(studentlistitems, 1);
+appendPageLinks(studentlistitems);
+
 
 
 
--- readme.md
+++ readme.md
@@ -2,3 +2,17 @@ Hiding and showing HTML on a page, using a set of buttons that you add to the pa
 
 The two main parts of this project that I need to complete to get a Meets Expectations grade require me to add buttons to the bottom of the page and show different sets of student information in list of 10 with the last page being 4 students when each button is clicked.
 
+ I made the necesary changes below to my code in  the script.js file and was able to get it to run scuccessfully. 
+
+ Edit suggestions from slack unit 2 chanel
+ 
+You have an error in your for loop you have <= list.length; but that should be < list.length;  If you change that then it fixes an TypeError you are getting. 
+In your  appendPageLinks function you are trying to get the div but  it doesn't exist yet. You should be creating it. 
+You are appending the div to parentDiv with the page class  You are using querySelectAll to get the .page div but you should use document.querySelector since you are just bringing back one element.
+ You are trying to select the ul but like the div you need to create it then you can append it to the div you created.
+In line 83 you have a typo you wrote inks instead of links 
+In lines 77 and 80  you need to use createElement because the those elements don't exist yet so you need to create them then you can select them or add attributes to them.
+After those changes are done at the bottom of your script you can call your functions like this
+showPage(studentlistitems,1)
+appendPageLinks(studentlistitems)
+and your everything should work! Hope that helps you! A lot of the bugs turned out to be that you were trying to select elements that did not exist yet. One way to check is to console.log the element you are selecting. If it shows up undefined or null then you know something is wrong.  You can also look at the HTML to get an idea of what elements need to be created and which ones already exist. Keep up the awesome work :grinning: (edited)
